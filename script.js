73afcfb1e903ccd9c15e9292381961042990b98d
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2021-02-18 09:58:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ var links="";
 for (var i=0; i<document.getElementsByClassName("fileThumb").length; i++) {
 	links = links + document.getElementsByClassName("fileThumb")[i].href + "<br>";
 }
-document.write(links);
+document.write(links); document.body.style.fontFamily="Lucida Console, monospace";
